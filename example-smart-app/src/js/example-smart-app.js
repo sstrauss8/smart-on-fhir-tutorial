a8cc42c6c4794e422abc7e5f7b0afc449cefd649
sstrauss8 <s.strauss8@gmail.com>
2019-10-17 21:49:02 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -42,7 +42,7 @@
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
-          //var weight = byCodes('29463-7');
+          var weight = byCodes('29463-7');
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
@@ -50,7 +50,7 @@
           p.fname = fname;
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
-          //p.weight = getQuantityValueAndUnit(weight[0]);
+          p.weight = getQuantityValueAndUnit(weight[0]);
 
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
@@ -86,7 +86,7 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
-      //weight: {value: ''},
+      weight: {value: ''},
     };
   }
 
@@ -130,7 +130,7 @@
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
-    //$('#weight').html(p.weight);
+    $('#weight').html(p.weight);
   };
 
 })(window);
